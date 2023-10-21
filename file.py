77f61e6163e39b4d48d1e2761d647aff073b593d
xeno <arjungr8898@gmail.com>
2023-10-21 10:45:23 +0530

    git actions practice

---
+++ file.py
@@ -0,0 +1,6 @@
+def helloProgram():
+  return("Hello world")
+
+
+if __name__ == "__main__":
+  helloProgram()
---
+++ test_file.py
@@ -0,0 +1,4 @@
+from file import helloProgram
+
+def test_helloProgram():
+  assert helloProgram() == "Hello world"
